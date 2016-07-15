2a6497ad61af9aa7cd1e43ea79606f4f0603f3cc
Alexandre <alexandre@lalalab.com>
2016-07-15 17:10:30 +0200

    create ClassMap loader class

---
+++ src/Autoloadr/ClassMap.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Autoloadr;
+
+
+/**
+ * Class Autoloadr\ClassMap
+ * This is a basic autoload-map loader
+ *
+ * @author alex.robert
+ * @package Autoloadr
+ */
+Class ClassMap{
+
+    
+
+
+}
